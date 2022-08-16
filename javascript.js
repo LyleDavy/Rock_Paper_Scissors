b9397f66f2cbd4261355992ef32bee50a2160e8e
Lyle <lylecdavid@gmail.com>
2022-08-16 09:43:38 -0400

    add playRound function previous unlabelled commit - add computerChoice function

--- javascript.js
+++ javascript.js
@@ -3,3 +3,21 @@ function getComputerChoice() {
     let computerChoice = choices[Math.floor(Math.random() * choices.length)];
     return computerChoice;
 }
+
+let question = prompt('Rock, Paper, or Scissors?');
+let playerSelection = question.toLowerCase();
+let computerSelection = getComputerChoice();
+
+function playRound(playerSelection,computerSelection) {
+    if (playerSelection == computerSelection) {
+        return(`It's a tie! The computer picked ${computerSelection}`)
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return('You win! Rock beats scissors.')
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return('You win! Paper beats rock.')
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return('You win! Scissors beats paper.')
+    } else {
+        return(`You lose! ${computerSelection} beats ${playerSelection}.`)
+    }
+}
