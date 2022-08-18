4c6d223f365895959103a36117070e911bd1350c
Lyle <lylecdavid@gmail.com>
2022-08-18 17:20:17 -0400

    Add 5 round game function with score

--- javascript.js
+++ javascript.js
@@ -7,17 +7,33 @@ function getComputerChoice() {
 let question = prompt('Rock, Paper, or Scissors?');
 let playerSelection = question.toLowerCase();
 let computerSelection = getComputerChoice();
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
 
 function playRound(playerSelection,computerSelection) {
     if (playerSelection == computerSelection) {
         return(`It's a tie! The computer picked ${computerSelection}`)
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        playerScore++;
         return('You win! Rock beats scissors.')
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        playerScore++;
         return('You win! Paper beats rock.')
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return('You win! Scissors beats paper.')
+        playerScore++;
+        return('You win! Scissors beats paper.');
     } else {
+        computerScore++;
         return(`You lose! ${computerSelection} beats ${playerSelection}.`)
     }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let question = prompt('Rock, Paper, or Scissors?');
+        const playerSelection = question.toLowerCase();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection,computerSelection));
+    }
+    return(`Player Score: ${playerScore}        Computer Score: ${computerScore}`)
 }
